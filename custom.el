d1c87eee9a8e759415a2700c3805ee33b1cf55df
Pedro Castilho <cstl.px@gmail.com>
2017-07-24 03:48:15 -0300

    Adding Clojure + enhancements

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (typescript-mode graphql-mode flycheck-scala-sbt sbt-mode scala-mode flycheck-rust json-mode toml-mode yaml-mode flycheck-swift3 swift-mode flymake-rust rust-mode flycheck-kotlin kotlin-mode exec-path-from-shell gitignore-mode flycheck flycheck-flow auctex wakatime-mode js2-mode))))
+    (cider clojure-mode clojure-mode-extra-font-locking ido-ubiquitous magit paredit projectile rainbow-delimiters smex tagedit typescript-mode graphql-mode flycheck-scala-sbt sbt-mode scala-mode flycheck-rust json-mode toml-mode yaml-mode flycheck-swift3 swift-mode flymake-rust rust-mode flycheck-kotlin kotlin-mode exec-path-from-shell gitignore-mode flycheck flycheck-flow auctex wakatime-mode js2-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -22,28 +22,114 @@ The return value is the new value of LIST-VAR."
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(defvar my-packages)
+(setq my-packages
+      '(cider queue spinner clojure-mode clojure-mode-extra-font-locking
+              clojure-mode ido-ubiquitous ido-completing-read+ magit git-commit
+              with-editor magit-popup async paredit projectile rainbow-delimiters
+              smex tagedit s exec-path-from-shell flycheck-kotlin flycheck-rust
+              flycheck-scala-sbt flycheck-swift3 flycheck dash flymake-rust
+              flymake-easy graphql-mode json-mode json-reformat json-snatcher
+              kotlin-mode pkg-info epl rust-mode sbt-mode scala-mode seq
+              swift-mode toml-mode typescript-mode wakatime-mode yaml-mode))
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package my-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
 ;;----------------------------------------------------------
 
 ;;Make sure we get the correct environment variables---------
 (exec-path-from-shell-initialize)
 ;;-----------------------------------------------------------
 
-;;JS mode---------------------------------------------------
+;;ELisp-------------------------------------------------------
+;; Automatically load paredit when editing a lisp file
+;; More at http://www.emacswiki.org/emacs/ParEdit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;; eldoc-mode shows documentation in the minibuffer when writing code
+;; http://www.emacswiki.org/emacs/ElDoc
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
+(add-hook 'emacs-lisp-mode-hook       'rainbow-delimiters-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook 'rainbow-delimiters-mode)
+(add-hook 'ielm-mode-hook             'rainbow-delimiters-mode)
+(add-hook 'lisp-mode-hook             'rainbow-delimiters-mode)
+(add-hook 'lisp-interaction-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'scheme-mode-hook           'rainbow-delimiters-mode)
+;;-------------------------------------------------------------
+
+;;JS mode/HTML mode---------------------------------------------------
 (require 'js)
 (setq js-indent-level 2)
+(add-hook 'js-mode-hook 'subword-mode)
+(add-hook 'js-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'html-mode-hook 'subword-mode)
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js-mode))
+(eval-after-load "sgml-mode"
+  '(progn
+     (require 'tagedit)
+     (tagedit-add-paredit-like-keybindings)
+     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
 ;;----------------------------------------------------------
 
 ;;TypeScript------------------------------------------------
+(add-hook 'typescript-mode-hook 'rainbow-delimiters-mode)
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 ;;----------------------------------------------------------
 
 ;;Python mode-----------------------------------------------
-(require 'python)
 (setq-default python-indent-offset 2)
 ;;----------------------------------------------------------
 
+;;Clojure---------------------------------------------------
+(require 'cider-mode)
+
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(add-hook 'clojure-mode-hook 'subword-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (setq inferior-lisp-program "lein repl")
+            (font-lock-add-keywords
+             nil
+             '(("(\\facts?\\)"
+                (1 font-lock-keyword-face))
+               ("(\\background?\\)"
+                (1 font-lock-keyword-face))))
+            (define-clojure-indent (fact 1))
+            (define-clojure-indent (facts 1))))
+
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq cider-repl-pop-to-buffer-on-connect t)
+(setq cider-show-error-buffer t)
+(setq cider-auto-select-error-buffer t)
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+(setq cider-repl-wrap-history t)
+
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("lein-env" . ruby-mode))
+;;----------------------------------------------------------
+
+;;Shell----------------------------------------------------
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+;;----------------------------------------------------------
  
 ;;Wakatime--------------------------------------------------
 (require 'wakatime-mode)
@@ -66,7 +152,21 @@ The return value is the new value of LIST-VAR."
 (require 'ido)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-(ido-mode 1)
+(setq ido-use-virtual-buffers t)
+(setq ido-use-filename-at-point nil)
+(setq ido-auto-merge-work-directories-length -1)
+(ido-mode t)
+(ido-ubiquitous-mode 1)
+;;----------------------------------------------------------
+
+;;SMEX------------------------------------------------------
+(require 'smex)
+(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+(global-set-key (kbd "C-x C-b") 'smex)
+;;----------------------------------------------------------
+
+;;Projectile----------------------------------------------
+(projectile-mode)
 ;;----------------------------------------------------------
 
 ;;misc editing stuff----------------------------------------
@@ -75,6 +175,7 @@ The return value is the new value of LIST-VAR."
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (turn-on-font-lock)
 (column-number-mode)
+(fset 'yes-or-no-p 'y-or-n-p)
 ;;International keyboards
 (require 'iso-transl)
 ;;----------------------------------------------------------
